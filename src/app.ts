d1f262f2b42ee4b591702c5ef832e97e27d5f3fd
Guilherme Rocha <guirocha1938@gmail.com>
2024-08-08 20:20:53 -0300

    feat: Introduces advanced types and features.

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,120 @@
-type AddFn = (a: number, b: number) => number;
-/* interface AddFn {
-    (a: number, b: number): number;
-} */
+type Admin = {
+    name: string;
+    privileges: string[];
+};
 
-let add : AddFn;
+type Employee = {
+    name: string;
+    startDate: Date;
+};
 
-add = (n1: number, n2: number) => {
-    return n1 + n2;
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: "Rocha",
+    privileges: ["To rule them all"],
+    startDate: new Date()
 }
 
-interface Named {
-    readonly name?: string;
-    outputName?: string;
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function Add(a: number, b: number): number
+function Add(a: string, b: string): string
+function Add(a: string, b: number): string
+function Add(a: number, b: string): string
+function Add(a: Combinable, b: Combinable) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    } else {
+        return a + b;
+    }
 }
 
-interface Greetable extends Named {
-    greet(phrase: string): void;
+const result = Add("Rocha", "Jaquelinda");
+result.split(' ');
+
+const fetchedUserData = {
+    id: "0",
+    userName: "Rocha",
+    job: { title: "CEO", description: "My own company" }
+};
+// optional chainning is the "?" inside log.
+console.log(fetchedUserData?.job?.title);
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation (emp: UnknownEmployee) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges" + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Privileges" + emp.startDate);
+    }
 }
 
-class Person implements Greetable {
-    name?: string;
-    age = 30;
+printEmployeeInformation(e1);
 
-     constructor(n?: string) {
-        if (n) {
-            this.name = n;
-        }
-     }
-
-     greet (phrase: string) {
-        if (this.name) {
-            console.log(phrase + " " + this.name);
-        } else {
-            console.log("Hi!");
-        }
-     }
+class Car {
+    drive() {
+        console.log("Driving a car");
+    }
 }
 
-let user1: Greetable;
+class Truck {
+    drive() {
+        console.log("Driving a truck");
+    }
 
-user1 = new Person();
-user1.greet("Hi there, I am");
+    loadCargo() {
+        console.log("Loading cargo...");
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle (vehicle: Vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo();
+    }
+}
+
+interface Bird {
+    type: "bird";
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: "horse";
+    runningSpeed: number
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    switch (animal.type) {
+        case "bird":
+            console.log("Flying at speed: " + animal.flyingSpeed);
+            break;
+        case "horse":
+            console.log("Running at speed: " + animal.runningSpeed);
+            break;
+    }
+}
+
+moveAnimal({ type: "horse", runningSpeed: 8 });
+
+interface ErrorContainer {
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    1: "Not a valid email",
+}
